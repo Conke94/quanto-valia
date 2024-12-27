90e98832f096ac0f8153d3c3b409d6c2f72f74aa
Conke94 <gabrielmoroconke@hotmail.com>
2024-12-26 22:45:25 -0300

    fix: cleaning stuffs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app/index.css';
-import { App } from './app/App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <></> {/*Components rendered initally*/}
   </React.StrictMode>
 );
--- frontend/src/pages/Login/components/ForgotPassword.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function ForgotPassword(){
-    return(
-        <div>
-            <a href='#' className='text-blue-700'>Forgot password</a>
-        </div>
-    )
-}
