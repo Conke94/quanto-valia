2cb8ef432ea475fa7f371a7bdcfc0e2bad15b61d
Conke94 <gabrielmoroconke@hotmail.com>
2025-03-12 17:20:00 -0300

    feat: set app to class

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,22 @@ import cors from 'cors';
 import { connectDatabase } from './database/utils/connect-database.js';
 import routes from './api/routes/index.js';
 
-const app = express();
+class App {
+    constructor(){ this.app = express(); }
 
-app.use(express.json());
+    createInstance(){
+        this.app.use(express.json());
 
-app.use(cors());
-app.use(cors({origin: 'http://localhost:3000'}));
-app.use('/standard', routes);
+        this.app.use(cors());
+        this.app.use(cors({origin: 'http://localhost:3000'}));
+        this.app.use('/standard', routes);
 
-app.listen(5000 ,async () => {
-  console.log(`Servidor rodando na porta 5000`);
-  await connectDatabase()
-});
+        this.app.listen(5000 ,async () => {
+            console.log(`Servidor rodando na porta 5000`);
+            await connectDatabase()
+        });
+    }
+}
 
+const server = new App();
+server.createInstance();
